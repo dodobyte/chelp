facd744a2a8960753560b7d008327cd654031ff9
Dogan Kurt <dogan.kurt@dodobyte.com>
2018-04-13 11:51:17 +0200

    dns --

--- chelp.h
+++ chelp.h
@@ -3,9 +3,6 @@
 
 #include <windows.h>
 
-// dns.c
-BOOL RetrieveDNS(LPWSTR pszAdapterName, CHAR *dns);
-
 // lz4.c
 int LZ4_decompress_safe(const char* src, char* dst, int compressedSize, int dstCapacity);
 
